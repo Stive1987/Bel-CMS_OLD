691232589794b5cd2c0455999f4ff74f8c89a842
Determe Stive <stivedeterme@gmail.com>
2014-10-22 13:18:56 +0200

    create folders and index and htaccess

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Bel-CMS [Content management system]
+ * @version 0.0.1
+ * @link http://www.bel-cms.be
+ * @link http://www.stive.eu
+ * @license http://opensource.org/licenses/GPL-3.0 copyleft
+ * @copyright 2014 Bel-CMS
+ * @author Stive - mail@stive.eu
+ */
+
+$start_microtime = microtime(true);
+
+$end_microtime   = round(microtime(true) - $start_microtime, 3).' s';
