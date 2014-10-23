9d903b826f7c20d440c2d07b23e2637772924c40
Determe Stive <stivedeterme@gmail.com>
2014-10-23 23:03:10 +0200

    add class define

---
+++ core/define.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Bel-CMS [Content management system]
+ * @version 0.0.1
+ * @link http://www.bel-cms.be
+ * @link http://www.stive.eu
+ * @license http://opensource.org/licenses/GPL-3.0 copyleft
+ * @copyright 2014 Bel-CMS
+ * @author Stive - mail@stive.eu
+ */
+
+class Define
+{
+	public function constant ($array = array())
+	{
+		foreach ($array as $constant => $tableName) {
+			if (!defined($constant)) {
+    			define($constant, $tableName);
+    		}
+		}
+	}
+}
+$define = new Define();
