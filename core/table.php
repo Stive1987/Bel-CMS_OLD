fc7b11adc48683f467133c8af89de49fbe465677
Determe Stive <stivedeterme@gmail.com>
2014-10-26 00:51:39 +0200

    add file table.php + add array table BDD

---
+++ core/table.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Bel-CMS [Content management system]
+ * @version 0.0.1
+ * @link http://www.bel-cms.be
+ * @link http://www.stive.eu
+ * @license http://opensource.org/licenses/GPL-3.0 copyleft
+ * @copyright 2014 Bel-CMS
+ * @author Stive - mail@stive.eu
+ */
+
+if (basename(__FILE__) == basename($_SERVER['SCRIPT_FILENAME'])) {
+	die('Direct access forbidden');
+}
+
+$array =   array(
+	'TABLE_COMMENTS'       => 'comments',
+	'TABLE_CONFIG'         => 'config',
+	'TABLE_FORUM'          => 'forum',
+	'TABLE_FORUM_CAT'      => 'forum_cat',
+	'TABLE_FORUM_POST'     => 'forum_post',
+	'TABLE_FORUM_THREADS'  => 'forum_threads',
+	'TABLE_GROUPS'         => 'groups',
+	'TABLE_ACTION'         => 'infos_action',
+	'TABLE_MAIL_BLACKLIST' => 'mails_blacklist',
+	'TABLE_NEWS'           => 'news',
+	'TABLE_USERS'          => 'users'
+);
+$define -> constant ($array);
+?>
