1746c8f0bc7f5b533810c99b9b11c4cdef93e77d
Determe Stive <stivedeterme@gmail.com>
2014-10-26 00:55:59 +0200

    add file lang.fr.php

---
+++ lang/lang.fr.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Bel-CMS [Content management system]
+ * @version 0.0.1
+ * @link http://www.bel-cms.be
+ * @link http://www.stive.eu
+ * @license http://opensource.org/licenses/GPL-3.0 copyleft
+ * @copyright 2014 Bel-CMS
+ * @author Stive - mail@stive.eu
+ */
+define ('ERROR'   , 'Erreur');
+define ('WARNING' , 'Avertissement');
+define ('INFO'    , 'Information');
+define ('SUCCESS' , 'Succès');
