5fd1f66c8ee16e3a8f0a8168462fff9d0b095728
Determe Stive <stivedeterme@gmail.com>
2014-10-26 00:48:39 +0200

    update include + password.php + error.class.php + lang.fr.php + table.php + common.class.php + config.class.php + users.class.php

--- assets/include.php
+++ assets/include.php
@@ -11,8 +11,17 @@
 
 if (basename (__FILE__) == basename ($_SERVER['SCRIPT_FILENAME'])) { die ('Direct access forbidden'); }
 
+if (!function_exists('password_hash')) {
+	require_once ROOT.'core/password.php';
+}
 require_once ROOT.'assets/function.php';
+require_once ROOT.'core/error.class.php';
 require_once ROOT.'core/define.class.php';
+require_once ROOT.'lang/lang.fr.php';
+require_once ROOT.'core/table.php';
+require_once ROOT.'core/common.class.php';
 require_once ROOT.'core/pdo.class.php';
 require_once ROOT.'core/geturl.class.php';
+require_once ROOT.'core/config.class.php';
+require_once ROOT.'core/users.class.php';
 require_once ROOT.'core/dispatcher.class.php';
