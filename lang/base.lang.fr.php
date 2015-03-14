1c48fac610c30b8b0e1f43f01d50751ffc9d728a
Determe Stive <stivedeterme@msn.com>
2015-03-14 23:32:00 +0100

    add file + base.lang.fr.php

---
+++ lang/base.lang.fr.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Bel-CMS [Content management system]
+ * @version 0.0.1
+ * @link http://www.bel-cms.be
+ * @link http://www.stive.eu
+ * @license http://opensource.org/licenses/GPL-3.0 copyleft
+ * @copyright 2014 Bel-CMS
+ * @author Stive - mail@stive.eu
+ */
+
+Common::constant(array(
+	#####################################
+	# Fichier lang en français - Erreur
+	#####################################
+	'ERROR'           => 'Erreur',
+	'WARNING'         => 'Avertissement',
+	'INFO'            => 'Information',
+	'SUCCESS'         => 'Succès',
+    #####################################
+    # COMMUN
+    #####################################
+    'VALIDATE_MEMBER' => 'Membre validé',
+    'VALIDATE_MEMBER' => 'Membre en actif',
+    'PENDINNG_MEMBER' => 'Membre en attente',
+    'UNKNOWN'         => 'Inconnu',
+    'GUEST'           => 'Visiteur',
+    'VALID'           => 'Valider',
+    'SEE'             => 'Voir',
+    'ADD'             => 'Ajouter',
+    'EDIT'            => 'Editer',
+    'MODIFY'          => 'Modifier',
+    'DELETE'          => 'Supprimer',
+    'BACK'            => 'Retour',
+    'CONFIRM'         => 'Confirmer',
+    'RED'             => 'Rouge',
+    'BLUE'            => 'Bleu',
+    'YELLOW'          => 'Jaune',
+    'GREEN'           => 'Vert',
+	#####################################
+	# POSITION
+	#####################################
+	'TOP'             => 'Haut',
+	'RIGHT'           => 'Droit',
+	'BOTTOM'          => 'Bas',
+	'LEFT'            => 'Gauche',
+    #####################################
+    # Administration
+    #####################################
+    'PREFGEN'         => 'Préférences Générales',
+    #####################################
+    # Nom des modules
+    #####################################
+    'NEWS'            => 'News',
+    'DOWNLOADS'       => 'Téléchargements',
+    'FORUM'           => 'Forum',
+    'USER'            => 'Utilisateur',
+    'USERS'           => 'Utilisateurs',
+    'COMMENTS'        => 'Commentaires',
+    'COMMENT'         => 'Commentaire',
+));
